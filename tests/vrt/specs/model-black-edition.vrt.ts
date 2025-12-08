c2b41833fdb83a8efd95982e9458cb5d0273d23f
Mihael Smrček <mihael.smrcek@porsche.digital>
2025-12-08 09:58:13 +0100

    feat(model): update vrt config | ms | feat/model

--- tests/vrt/specs/model-black-edition.vrt.ts
+++ tests/vrt/specs/model-black-edition.vrt.ts
@@ -3,5 +3,9 @@ import { expect, test } from '@playwright/test';
 test('has no visual regression', async ({ page }) => {
   await page.goto('/model-black-edition/');
 
-  await expect(page).toHaveScreenshot('model-black-edition.png', { fullPage: true });
+  await expect(page).toHaveScreenshot('model-black-edition.png', {
+    fullPage: true,
+    maxDiffPixels: 100,
+    threshold: 0.2,
+  });
 });
