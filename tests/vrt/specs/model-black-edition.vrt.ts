4f61df80f4a2760b7a7e93131df5dfaa41c7aad8
Mihael Smrček <mihael.smrcek@porsche.digital>
2025-12-08 10:07:40 +0100

    Revert "feat(model): update vrt config | ms | feat/model"
    This reverts commit c2b41833fdb83a8efd95982e9458cb5d0273d23f.

--- tests/vrt/specs/model-black-edition.vrt.ts
+++ tests/vrt/specs/model-black-edition.vrt.ts
@@ -3,9 +3,5 @@ import { expect, test } from '@playwright/test';
 test('has no visual regression', async ({ page }) => {
   await page.goto('/model-black-edition/');
 
-  await expect(page).toHaveScreenshot('model-black-edition.png', {
-    fullPage: true,
-    maxDiffPixels: 100,
-    threshold: 0.2,
-  });
+  await expect(page).toHaveScreenshot('model-black-edition.png', { fullPage: true });
 });
