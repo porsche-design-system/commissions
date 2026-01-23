0426d415cc6afc911d8baf09b2806f3b72cf0a2c
Mihael Smrček <mihael.smrcek@porsche.digital>
2026-01-23 11:59:43 +0100

    feat(model): remove animations | ms | feat/model

--- tests/vrt/specs/model-black-edition.vrt.ts
+++ tests/vrt/specs/model-black-edition.vrt.ts
@@ -3,5 +3,24 @@ import { expect, test } from '@playwright/test';
 test('has no visual regression', async ({ page }) => {
   await page.goto('/model-black-edition/');
 
+  await page.addStyleTag({
+    content: `
+            :root {
+              --p-animation-duration: 0s !important;
+              --p-transition-duration: 0s !important;
+              --transition-duration-short: 0s !important;
+              --transition-duration-moderate: 0s !important;
+              --transition-duration-long: 0s !important;
+              --transition-duration-very-long: 0s !important;
+            }
+            
+            * {
+              animation: none !important;
+              transition: none !important;
+            }
+            
+          `,
+  });
+
   await expect(page).toHaveScreenshot('model-black-edition.png', { fullPage: true });
 });
